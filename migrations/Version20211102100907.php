20d11eb3e1e8b67ba9af5dfccb81b92f88af70f3
Alexandre RAVICHANDRAN <alexandre.ravichandran@gmail.com>
2021-11-02 11:53:15 +0100

    back-alexandre-fix-fix sql database issue

--- migrations/Version20211102100907.php
+++ migrations/Version20211102100907.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20211102091148 extends AbstractMigration
+final class Version20211102100907 extends AbstractMigration
 {
     public function getDescription(): string
     {
